a52cabfb8b59501f05838da7043108c521485cb4
Giuseppe Ursino <giursino@gmail.com>
2018-02-25 17:53:48 +0100

    Aggiunta libreria per accesso a KNX da seriale

---
+++ include/libknx810.h
@@ -0,0 +1,84 @@
+//******************************************************************************
+/// @file libknx810.h
+/// @brief Header file del modulo libknx810
+/// $Author$
+/// $Date$
+/// $Revision$
+///
+///
+///
+//******************************************************************************
+
+//
+// -----
+// $Id$
+// -----
+//
+
+
+//-START--------------------------- Definitions ------------------------------//
+
+#ifdef LOCAL
+	#undef LOCAL
+#endif
+#define LOCAL static			///< Identifica tutti gli oggetti a carattere locale
+#ifdef GLOBAL
+	#undef GLOBAL
+#endif
+#define GLOBAL extern			///< Identifica tutti gli oggetti a carattere globale
+
+//-END----------------------------- Definitions ------------------------------//
+
+
+//-START------------------------------ Types ---------------------------------//
+///KNX DPT size
+typedef enum {
+	LK8_DPT_6BIT,
+	LK8_DPT_1BYTE,
+	LK8_DPT_2BYTE,
+	LK8_DPT_3BYTE,
+	LK8_DPT_4BYTE,
+	LK8_DPT_5BYTE,
+	LK8_DPT_6BYTE,
+	LK8_DPT_7BYTE,
+	LK8_DPT_8BYTE,
+	LK8_DPT_9BYTE,
+	LK8_DPT_10BYTE,
+	LK8_DPT_11BYTE,
+	LK8_DPT_12BYTE,
+	LK8_DPT_13BYTE,
+	LK8_DPT_14BYTE,
+	LK8_DPT_MAX,
+} LK8_DPT_TYPE;
+
+/// KNX address
+typedef union {
+	uint8_t		byte[2];
+	uint16_t	glb;
+} LK8_ADDR_TYPE;
+
+/// Max length of KNX message
+#define LK8_KNX_MSG_LENGTH		22
+//-END-------------------------------- Types ---------------------------------//
+
+//-START------------------------------ Macro ---------------------------------//
+//-END-------------------------------- Macro ---------------------------------//
+
+//-START------------------------------ Const ---------------------------------//
+//-END-------------------------------- Const ---------------------------------//
+
+//-START---------------------------- Variables -------------------------------//
+//-END------------------------------ Variables -------------------------------//
+
+//-START----------------------- Functions Declaration ------------------------//
+GLOBAL int LK8_Init(int* pDevice);
+GLOBAL int LK8_Deinit(int pDevice);
+GLOBAL int LK8_SendRawMessage(int pDevice, const uint8_t* pMsg, uint8_t u8MsgLen);
+GLOBAL int LK8_SendGroupValueWrite(int pDevice, LK8_ADDR_TYPE addr,
+		LK8_DPT_TYPE dpt, const uint8_t* payload, uint8_t len);
+GLOBAL int LK8_ReceiveRawMessage(int pDevice, uint8_t* pMsg, uint8_t u8MsgLen);
+
+//-END------------------------- Functions Declaration ------------------------//
+
+
+
---
+++ libknx810/libknx810_priv.h
@@ -0,0 +1,89 @@
+//******************************************************************************
+/// @file libknx810_priv.h
+/// @brief Header file del modulo libknx810
+/// $Author$
+/// $Date$
+/// $Revision$
+///
+///
+///
+//******************************************************************************
+
+//
+// -----
+// $Id$
+// -----
+//
+
+
+//-START--------------------------- Definitions ------------------------------//
+
+#ifdef LOCAL
+	#undef LOCAL
+#endif
+#define LOCAL static			///< Identifica tutti gli oggetti a carattere locale
+#ifdef GLOBAL
+	#undef GLOBAL
+#endif
+#define GLOBAL extern			///< Identifica tutti gli oggetti a carattere globale
+
+/// Max length of message
+#define LK8_MSG_LENGTH		32
+
+/// KNX msg field
+/// @{
+#define LK8_KNX_MSG_CTRLFIELD	0
+
+#define LK8_KNX_MSG_SRCADDR		LK8_KNX_MSG_SRCADDR1
+#define LK8_KNX_MSG_SRCADDR1	1
+#define LK8_KNX_MSG_SRCADDR2	2
+
+#define LK8_KNX_MSG_DSTADDR		LK8_KNX_MSG_DSTADDR1
+#define LK8_KNX_MSG_DSTADDR1	3
+#define LK8_KNX_MSG_DSTADDR2	4
+
+#define LK8_KNX_MSG_DAF			5
+#define LK8_KNX_MSG_LENFIELD	5
+#define LK8_KNX_MSG_NETFIELD	5
+
+#define LK8_KNX_MSG_TPCIFIELD	6
+#define LK8_KNX_MSG_APCIFIELD	LK8_KNX_MSG_APCIFIELD1
+#define LK8_KNX_MSG_APCIFIELD1	6
+#define LK8_KNX_MSG_APCIFIELD2	7
+
+#define LK8_KNX_MSG_PAYLOAD0	7
+#define LK8_KNX_MSG_PAYLOAD 	8
+/// @}
+
+/// KNX MSG Default Control Field
+#define LK8_DEF_CTRLFIELD		0xBC
+
+/// KNX MSG Default Source Address
+#define LK8_DEF_SRCADDR1		0x11
+#define LK8_DEF_SRCADDR2		0x0F
+
+/// KNX Destination address flag, map on byte 3
+#define LK8_DEF_DAF_GROUP		0xE0
+#define LK8_DEF_DAF_INDIVIDUAL	0x00
+
+/// APCI
+#define LK8_DEF_AGROUPWRITE 	0x0080
+//-END----------------------------- Definitions ------------------------------//
+
+
+//-START------------------------------ Types ---------------------------------//
+//-END-------------------------------- Types ---------------------------------//
+
+//-START------------------------------ Macro ---------------------------------//
+//-END-------------------------------- Macro ---------------------------------//
+
+//-START------------------------------ Const ---------------------------------//
+//-END-------------------------------- Const ---------------------------------//
+
+//-START---------------------------- Variables -------------------------------//
+//-END------------------------------ Variables -------------------------------//
+
+//-START----------------------- Functions Declaration ------------------------//
+//-END------------------------- Functions Declaration ------------------------//
+
+
