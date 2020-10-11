a8ddb089761b9e3f0aa4dea7b5be485f5737b3b3
Giuseppe Ursino <giursino@gmail.com>
2020-10-11 18:44:57 +0200

    enable bus-monitor mode on 01847 before start reading

--- example/01847-busmonitor-nolib.c
+++ example/01847-busmonitor-nolib.c
@@ -365,12 +365,15 @@ int main(int argc, char **argv)
 #include <windows.h>
 #endif
 #include <stdio.h>
+#include <stdint.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include <errno.h>
 #include <time.h>
+#include <signal.h>
+#include <stdbool.h>
 #include <hidapi/hidapi.h>
 
 
@@ -382,8 +385,33 @@ int main(int argc, char **argv)
 #define ANSI_COLOR_CYAN    "\x1b[36m"
 #define ANSI_COLOR_RESET   "\x1b[0m"
 
+#define CEMI_M_CODE_POS 11
+#define CEMI_PROP_DATA_POS 18
+
+#define CEMI_M_PropRead_req  0xFC
+#define CEMI_M_PropRead_con  0xFB
+
+#define CEMI_M_PropWrite_req 0xF6
+#define CEMI_M_PropWrite_con 0xF5
+
 
 #define MAX_STR 255
+
+static bool toexit = false;
+
+/// Signal handler, to exit from main loop
+static void SignalHandler(int signo) {
+	switch  (signo) {
+		case SIGINT:
+		case SIGTERM:
+			printf("Catched signal %i.\n", signo);
+			toexit=true;
+			break;
+		default:
+			break;
+	}
+
+}
 
 int main(int argc, char* argv[])
 {
@@ -441,17 +469,70 @@ int main(int argc, char* argv[])
 	printf("%i\n", res);
 	wprintf(L"Indexed String 1: %s\n", wstr);
 
+	// Flush buffer
+	printf("Flush buffer...\n");
+	do {
+		res = hid_read_timeout(handle, buf, 65, 1000);
+		printf(" flush %i bytes\n", res);
+	} while (res > 0);
+
+	// Enable bus-monitor mode
+	printf("Enable bus-monitor mode...");
+	const uint8_t msg_busmonitor_enable[] = {
+		0x01, 0x13, 0x10, 0x00, 0x08, 0x00, 0x08, 0x01, 0x03, 0x00, 0x00,
+		CEMI_M_PropWrite_req,
+		0x00, 0x08, 0x01, 0x34, 0x10, 0x01, 0x01
+	};
+	res = hid_write(handle, msg_busmonitor_enable, sizeof(msg_busmonitor_enable));
+	printf("%i\n", res);
+	printf("Waiting response...");
+	res = hid_read_timeout(handle, buf, 65, 1000);
+	printf("%i\n", res);
+	for (i = 0; i < res; i++) {printf("%.2hhx ", buf[i]); if (i==(res-1)) { printf("\n");};}
+	if ((res==64) && (buf[CEMI_M_CODE_POS]==CEMI_M_PropWrite_con)) {
+		printf("property written\n");
+	}
+
+	printf("Check bus-monitor mode...");
+	const uint8_t msg_busmonitor_get[] = {
+		0x01, 0x13, 0x0F, 0x00, 0x08, 0x00, 0x07, 0x01, 0x03, 0x00, 0x00,
+		CEMI_M_PropRead_req,
+		0x00, 0x08, 0x01, 0x34, 0x10, 0x01
+	};
+	res = hid_write(handle, msg_busmonitor_get, sizeof(msg_busmonitor_get));
+	printf("%i\n", res);
+	printf("Waiting response...");
+	res = hid_read_timeout(handle, buf, 65, 1000);
+	printf("%i\n", res);
+	for (i = 0; i < res; i++) {printf("%.2hhx ", buf[i]); if (i==(res-1)) { printf("\n");};}
+	if ((res==64) && (buf[CEMI_M_CODE_POS]==CEMI_M_PropRead_con) && (buf[CEMI_PROP_DATA_POS]==0x01)) {
+		printf("bus-monitor enabled!\n");
+	}
+
+
+	// Register signal handler
+	if (signal(SIGINT, SignalHandler) == SIG_ERR) {
+		perror("Cannot catch SIGINT");
+		exit(1);
+	}
+	if (signal(SIGTERM, SignalHandler) == SIG_ERR) {
+		perror("Cannot catch SIGTERM");
+		exit(1);
+	}
 
 	int len;
 
 	// Read requested state
 	printf("hid_read...\n");
-	while(1) {
-		res = hid_read(handle, buf, 65);
+	while(!toexit) {
+		res = hid_read_timeout(handle, buf, 65, 1000);
 
 		// Print out the returned buffer.
 		if (res < 0) {
 			perror("read");
+		}
+		else if (res == 0) {
+			continue;
 		}
 		else {
 			time_t timer;
@@ -494,6 +575,34 @@ int main(int argc, char* argv[])
 			}
 			puts("");
 		}
+	}
+
+	// Disable bus-monitor mode
+	printf("Disable bus-monitor mode...");
+	const uint8_t msg_busmonitor_disable[] = {
+		0x01, 0x13, 0x10, 0x00, 0x08, 0x00, 0x08, 0x01, 0x03, 0x00, 0x00,
+		CEMI_M_PropWrite_req,
+		0x00, 0x08, 0x01, 0x34, 0x10, 0x01, 0x00
+	};
+	res = hid_write(handle, msg_busmonitor_disable, sizeof(msg_busmonitor_disable));
+	printf("%i\n", res);
+	printf("Waiting response...");
+	res = hid_read_timeout(handle, buf, 65, 1000);
+	printf("%i\n", res);
+	for (i = 0; i < res; i++) {printf("%.2hhx ", buf[i]); if (i==(res-1)) { printf("\n");};}
+	if ((res==64) && (buf[CEMI_M_CODE_POS]==CEMI_M_PropWrite_con)) {
+		printf("property written\n");
+	}
+
+	printf("Check bus-monitor mode...");
+	res = hid_write(handle, msg_busmonitor_get, sizeof(msg_busmonitor_get));
+	printf("%i\n", res);
+	printf("Waiting response...");
+	res = hid_read_timeout(handle, buf, 65, 1000);
+	printf("%i\n", res);
+	for (i = 0; i < res; i++) {printf("%.2hhx ", buf[i]); if (i==(res-1)) { printf("\n");};}
+	if ((res==64) && (buf[CEMI_M_CODE_POS]==CEMI_M_PropRead_con) && (buf[CEMI_PROP_DATA_POS]==0x00)) {
+		printf("bus-monitor disabled!\n");
 	}
 
 	// Finalize the hidapi library
