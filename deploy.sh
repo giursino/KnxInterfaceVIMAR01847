1693594409705369ccfc5f3f14aca3cefde506e0
Giuseppe Ursino <giursino@gmail.com>
2020-10-11 23:18:45 +0200

    better deploy script

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,10 @@ set -e
 TARGET="pi@ggrasp"
 
 echo "Copying files to the TARGET..."
-scp libknxusb/.libs/libknxusb.so.0.0.0 example/.libs/01847-temp example/.libs/01847-busmonitor ${TARGET}:/tmp
+scp \
+	libknxusb/.libs/libknxusb.so.0.0.0 \
+	example/.libs/* \
+	${TARGET}:/tmp
 ssh ${TARGET} "cd /tmp; ln -sf libknxusb.so.0.0.0 libknxusb.so"
 ssh ${TARGET} "cd /tmp; ln -sf libknxusb.so.0.0.0 libknxusb.so.0"
 echo "Done."
